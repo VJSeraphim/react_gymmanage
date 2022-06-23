78bff48f81058e6ca954ead8cf03d0627fef9a5c
VJS <minsung_96@naver.com>
2022-06-23 23:58:30 +0900

    Search Exercises

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -21,13 +21,19 @@ const HeroBanner = () => {
         sx={{
           fontSize: {lg: '44px', xs:'40px'}
         }}
+        mb="23px" mt="30px"
       >
         Sweat, Smile <br/> and Repeat
       </Typography>
-      <Typography fontSize="220px" lineHeight="35px" mb={3}>
+      <Typography fontSize="220px" lineHeight="35px" mb={4}>
         Check out the most effective workouts for you!
       </Typography>
-      <Button variant="contained" color="error" href="#exercises">
+      <Button 
+        variant="contained" 
+        color="error" 
+        href="#exercises"
+        sx={{ backgroundColor: '#ff2625', padding: '10px'}}
+      >
         Explore Exercises
       </Button>
       <Typography
