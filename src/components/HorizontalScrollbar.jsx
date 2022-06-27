0875dd0c36a477eb69cf35e22fce32809b9f23ba
VJS <minsung_96@naver.com>
2022-06-27 23:56:15 +0900

    Scrollbar Arrow

--- src/components/HorizontalScrollbar.jsx
+++ src/components/HorizontalScrollbar.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import { Box } from '@mui/material'
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu'
 
-const HorizontalScrollbar = () => {
+import BodyPart from './BodyPart'
+
+const LeftArrow = () => {
+    const { scrollPrev } = useContext(VisibilityContext);
+  
+        return (
+        <Typography onClick={() => scrollPrev()} className="right-arrow">
+            <img src={LeftArrowIcon} alt="right-arrow" />
+        </Typography>
+        )
+  }
+  
+  const RightArrow = () => {
+    const { scrollNext } = useContext(VisibilityContext);
+  
+        return (
+        <Typography onClick={() => scrollNext()} className="left-arrow">
+            <img src={RightArrowIcon} alt="right-arrow" />
+        </Typography>
+        )
+  }
+
+const HorizontalScrollbar = ({ data, bodyParts, bodyPart, setBodyPart }) => {
     return (
-        <div>
+        <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
             {data.map((item) =>(
                 <Box
                     key={item.id || item}
@@ -11,10 +34,14 @@ const HorizontalScrollbar = () => {
                     title={item.id || item}
                     m="0 40px"
                 >
-                    {item}
+                    <BodyPart 
+                        item={item}
+                        bodyPart={bodyPart}
+                        setBodyPart={setBodyPart}
+                    />
                 </Box>
             ))}
-        </div>
+        </ScrollMenu>
     )
 }
 
