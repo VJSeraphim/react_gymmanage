1cd495750ad7d70dbbece7cc1ea14f4131bc7d64
VJS <minsung_96@naver.com>
2022-06-28 23:56:25 +0900

    Search Results

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Pagination from '@mui/material/Pagination'
+import { Box, Stack, Typography } from '@mui/material'
 
-const Exercises = () => {
+import { exerciseOptions, fetchData } from '../utils/fetchData'
+
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
   return (
-    <div>Exercises</div>
+    <Box
+      id="exercises"
+      sx={{mt: {lg: '110px'}}}
+      mt="50px"
+      p="20px"
+    >
+      <Typography>
+        Results
+      </Typography>
+      <Stack direction="row" sx={{ gap: {lg: '110px', xs: '50px'}}} flexWrap="wrap" justifyContent="center">
+        {exercises.map((exercise, i) => (
+          <p>
+            exercise.name
+          </p>
+        ))}
+      </Stack>
+    </Box>
   )
 }
 
