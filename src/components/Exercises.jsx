4b2a872e0db3822f4d111fa4a3e3fcef4426e632
VJS <minsung_96@naver.com>
2022-07-01 23:55:35 +0900

    Pagination Details

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -29,6 +29,8 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
         exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`, exerciseOptions)
       }
     }
+
+    fetchExercisesData()
   }, [bodyPart])
   
 
--- src/pages/ExerciseDetails.jsx
+++ src/pages/ExerciseDetails.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { Box } from '@mui/icons-material'
+
+import { exerciseOptions, fetchData } from '../utils/fetchData'
+
+import Detail from '../components/Detail'
+import ExerciseVideos from '../components/ExerciseVideos'
+import SimilarExercises from '../components/SimilarExercises'
 
 const ExerciseDetails = () => {
+    const [exerciseDetail, setExerciseDetail] = useState({})
+    const { id } = useParams()
+    
+    useEffect(() => {
+        const fetchExercisesData = async () => {
+            const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com'
+            
+        }
+
+        fetchExercisesData()
+    }, [id])
+    
     return (
-        <div>
-            ExerciseDetails
-        </div>
+        <Box>
+            <Detail exerciseDetail={exerciseDetail} />
+            <ExerciseVideos />
+            <SimilarExercises />
+        </Box>
     )
 }
 
