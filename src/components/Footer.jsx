06e388ad8943e2e3592b57bec11024bb896aa269
VJS <minsung_96@naver.com>
2022-07-09 23:51:48 +0900

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import { Box, Stack, Typography } from '@mui/material'
+
+import Logo from '../assets/images/Logo-1.png'
 
 const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
+        <Box mt="80px" bgcolor="#fff3f4">
+            <Stack 
+                gap="40px"
+                alignItems='center'
+                px="40px"
+                pt="24px"
+            >
+                <img 
+                    src={Logo} 
+                    alt="logo" 
+                    width="200px" 
+                    height="40px"
+                />
+                <Typography 
+                    variant="h5" 
+                    pb="40px"
+                    mt="20px"
+                >
+                    Random Copyright Words
+                </Typography>
+            </Stack>
+        </Box>
     )
 }
 
